c211ab2bea68e65aee727cdbe841436081ab2596
Alan Field <afield@redhat.com>
2018-06-08 07:30:48 -0700

    JDG-2004 PolarionJUnitXMLReporter is out of sync with upstream version
    https://issues.jboss.org/browse/JDG-2004
    
    Synchronizing this file with the upstream version to try and get rid of
    discrepancies between test suite results

--- commons-test/src/main/java/org/infinispan/commons/test/PolarionJUnitXMLReporter.java
+++ commons-test/src/main/java/org/infinispan/commons/test/PolarionJUnitXMLReporter.java
@@ -133,14 +133,16 @@ public class PolarionJUnitXMLReporter implements IResultListener2, ISuiteListene
     * @see org.testng.IConfigurationListener#onConfigurationFailure(org.testng.ITestResult)
     */
    @Override
-   public void onConfigurationFailure(ITestResult itr) {
+   public void onConfigurationFailure(ITestResult tr) {
+      checkDuplicatesAndAdd(tr);
+      m_numFailed.incrementAndGet();
    }
 
    /**
     * @see org.testng.IConfigurationListener#onConfigurationSkip(org.testng.ITestResult)
     */
    @Override
-   public void onConfigurationSkip(ITestResult itr) {
+   public void onConfigurationSkip(ITestResult tr) {
    }
 
    /**
@@ -187,7 +189,7 @@ public class PolarionJUnitXMLReporter implements IResultListener2, ISuiteListene
       document.pop();
 
       // Reset output directory
-      Utils.writeUtf8File(suite.getOutputDirectory().replaceAll("/Surefire suite", ""),
+      Utils.writeUtf8File(suite.getOutputDirectory().replaceAll(".Surefire suite", ""),
             generateFileName(suite) + ".xml", document.toXML());
    }
 
